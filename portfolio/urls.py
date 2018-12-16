d512dfe5d11e94503e2a21df5af490c18d261adb
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2018-12-16 20:38:30 +0600

    set portfolio html view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from .views import PortfolioView
+
 urlpatterns = [
+    path('', PortfolioView.as_view(), name='portfolio')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
 
 # Create your views here.
+class PortfolioView(DetailView):
+    template_name = 'portfolio/portfolio.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
